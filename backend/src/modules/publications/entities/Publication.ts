0ea360bcba7574d19ba096359f2da17e24247a73
NicholasTavares <nicholasbalby@hotmail.com>
2022-05-15 12:06:16 -0300

    refactor: adapt relationship with User and Comment entities and add new columns

--- backend/src/modules/publications/entities/Publication.ts
+++ backend/src/modules/publications/entities/Publication.ts
@@ -2,6 +2,7 @@ import User from '../../users/entities/User';
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { IPublication } from "../models/IPublication";
 import Comment from '@modules/comments/entities/Comment';
+import { IGeometry } from '../models/IGeometry';
 
 
 @Entity('publications')
@@ -10,14 +11,26 @@ class Publication implements IPublication {
   @PrimaryGeneratedColumn('uuid')
   id: string
 
-  @Column()
-  title: string;
-
-  @Column()
-  description: string;
-
   @ManyToOne(() => User, user => user.publications)
-  owner: User;
+  user_id: string
+
+  @Column()
+  title: string
+
+  @Column()
+  description: string
+
+  @Column()
+  pet_name: string
+
+  @Column()
+  gender: string
+
+  @Column()
+  disappearance_date: Date
+
+  @Column({type: "geometry"})
+  last_location: IGeometry
 
   @OneToMany(() => Comment, comment => comment.publication)
   comments: Comment[]
--- backend/src/modules/publications/models/IPublication.ts
+++ backend/src/modules/publications/models/IPublication.ts
@@ -1,9 +1,15 @@
-import User from "@modules/users/entities/User"
-
+import Comment from "@modules/comments/entities/Comment"
+import { IGeometry } from "./IGeometry"
 export interface IPublication {
+  id: string
+  user_id: string
   title: string
   description: string
-  owner: User
+  pet_name: string
+  gender: string
+  disappearance_date: Date
+  last_location: IGeometry
+  comments: Comment[]
   created_at: Date
   updated_at: Date
   deleted_at: Date
