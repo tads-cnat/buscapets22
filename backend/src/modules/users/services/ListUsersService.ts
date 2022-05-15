cc33df7fb134959e10cc0a070cf1a72dc128065a
NicholasTavares <nicholasbalby@hotmail.com>
2022-05-15 16:16:04 -0300

    refactor: add correct interface no list users service

--- backend/src/modules/users/services/ListUsersService.ts
+++ backend/src/modules/users/services/ListUsersService.ts
@@ -1,10 +1,10 @@
 import { getCustomRepository } from "typeorm";
-import { IUser } from "../models/IUser";
+import { IListUsers } from "../models/IListUsers";
 import UsersRepository from "../repositories/UsersRepository";
 
 class ListUsersService {
-  public async execute(): Promise<IUser[] | undefined> {
-    const usersRepository = getCustomRepository(UsersRepository);
+  public async execute(): Promise<IListUsers> {
+    const usersRepository = getCustomRepository(UsersRepository)
     const users = await usersRepository.findAll()
 
     return users
