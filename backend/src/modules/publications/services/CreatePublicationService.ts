f59f9a90457ffbac7c73607968f24490ee1ac4a8
NicholasTavares <nicholasbalby@hotmail.com>
2022-05-15 21:57:10 -0300

    refactor: adapt create publication service to new fields and geojson

--- backend/src/modules/publications/services/CreatePublicationService.ts
+++ backend/src/modules/publications/services/CreatePublicationService.ts
@@ -2,15 +2,33 @@ import { getCustomRepository } from 'typeorm';
 import { ICreatePublication } from '../models/ICreatePublication';
 import { IPublication } from '../models/IPublication';
 import PublicationRepository from '../repositories/PublicationsRepository';
+import { Geometry } from 'geojson'
 
 class CreatePublicationService {
-  public async execute({ title, description, owner }: ICreatePublication): Promise<IPublication> {
-    const publicationRepository = getCustomRepository(PublicationRepository);
+  public async execute({ 
+    user_id,
+    title,
+    description,
+    pet_name,
+    gender,
+    disappearance_date,
+    last_location 
+  }: ICreatePublication): Promise<IPublication> {
+    const publicationRepository = getCustomRepository(PublicationRepository)
+
+    const location: Geometry = {
+      type: 'Point',
+      coordinates: last_location
+   }
 
     const publication = await publicationRepository.create({
+      user_id,
       title,
       description,
-      owner
+      pet_name,
+      gender,
+      disappearance_date,
+      last_location: location
     })
 
     await publicationRepository.save(publication)
