30e5e0fc26399e17423989a3fb5ce4fe5010187e
NicholasTavares <nicholasbalby@hotmail.com>
2022-05-16 19:06:37 -0300

    refactor: remove not null validator and adapt find publication service to new interface

--- backend/src/modules/publications/services/FindPublicationService.ts
+++ backend/src/modules/publications/services/FindPublicationService.ts
@@ -1,16 +1,14 @@
-import AppError from "@shared/errors/AppErrors";
 import { getCustomRepository } from "typeorm";
+import { IFindById } from "../models/IFindById";
 import { IPublication } from "../models/IPublication";
 import PublicationRepository from "../repositories/PublicationsRepository";
 
 class FindPublicationService {
-  public async execute(id: string): Promise<IPublication> {
+  public async execute({id}: IFindById): Promise<IPublication> {
     const publicationRespository = getCustomRepository(PublicationRepository)
-    const publication = await publicationRespository.findById(id)
-
-    if (!publication) {
-      throw new AppError('Publication not found')
-    }
+    const publication = await publicationRespository.findById({
+      id
+    })
 
     return publication
   }
