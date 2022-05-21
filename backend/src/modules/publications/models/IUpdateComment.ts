5fc6e2807d75b51337125d43e7aaaf6316a60c6c
NicholasTavares <nicholasbalby@hotmail.com>
2022-05-20 21:19:04 -0300

    refactor: adapt update comment service to new interface

--- backend/src/modules/publications/models/IUpdateComment.ts
+++ backend/src/modules/publications/models/IUpdateComment.ts
@@ -1,4 +1,5 @@
 export interface IUpdateComment {
+  publication_id: string
   id: string
   comment: string
 }
--- backend/src/modules/publications/services/UpdateCommentService.ts
+++ backend/src/modules/publications/services/UpdateCommentService.ts
@@ -1,18 +1,16 @@
-import AppError from "@shared/errors/AppErrors";
 import { getCustomRepository } from "typeorm";
 import { IComment } from "../models/IComment";
 import { IUpdateComment } from "../models/IUpdateComment";
 import CommentsRepository from "../repositories/CommentsRepository";
 
 class UpdateCommentService {
-  public async update({id, comment}: IUpdateComment): Promise<IComment | undefined> {
+  public async update({publication_id ,id, comment}: IUpdateComment): Promise<IComment> {
     const commentRespository = getCustomRepository(CommentsRepository)
 
-    const commentFound = await commentRespository.findById(id)
-
-    if (!commentFound) {
-      throw new AppError('Comment not found')
-    }
+    const commentFound = await commentRespository.findByIdAndPublicationId({
+      publication_id,
+      id
+    })
 
     commentFound.comment = comment
 
